6d6d435cc8b70cc83c42a09a5804fed82a389c93
svasenkov <aanher@gmail.com>
2022-03-23 17:57:59 +0300

    updated library versions

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -6,9 +6,9 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class SearchTests {
     @Test
-    void selenideSearchTest() {
+    void successfulSearchTest() {
         open("https://www.google.com/");
         $("[name=q]").setValue("selenide").pressEnter();
-        $("[id=search]").shouldHave(text("selenide.org"));
+        $("[id=search]").shouldHave(text("https://selenide.org"));
     }
 }
