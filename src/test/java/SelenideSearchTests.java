4f841b0f87fd73e5fcd593ad23cb4046611132b9
svasenkov <aanher@gmail.com>
2024-08-30 15:22:10 +0200

    added Playwright, Selenium examples

--- src/test/java/SelenideSearchTests.java
+++ src/test/java/SelenideSearchTests.java
@@ -4,7 +4,7 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-public class SearchTests {
+public class SelenideSearchTests {
     @Test
     void successfulSearchTest() {
         open("https://www.google.com/");
