6cfedcebe495c5c90542c217d1b59206f2224088
svasenkov <aanher@gmail.com>
2026-09-10 21:18:37 +0200

    added captcha test

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -1,13 +1,25 @@
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 
 public class SearchTests {
     @Test
+    @Disabled("Get anti-captcha from developers, issue: @Jira-52343")
     void successfulSearchTest() {
         open("https://www.google.com/");
         $("[name=q]").setValue("selenide").pressEnter();
         $("[id=search]").shouldHave(text("https://selenide.org"));
+    }
+
+
+    @Test
+    void captchaShouldBeShownTest() {
+        open("https://www.google.com/");
+        $("[name=q]").setValue("selenide").pressEnter();
+        $("[id=captcha-form]").shouldBe(visible);
+        $("body").shouldHave(text("About this page"));
     }
 }
